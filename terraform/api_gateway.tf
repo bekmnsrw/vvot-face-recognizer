734bbb5735f43a15812538559aba92d37b6274b9
bekmnsrw <ilya.bekmansurov@gmail.com>
2025-01-25 21:19:59 +0300

    Fixed api_gateway spec

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -13,23 +13,23 @@ resource "yandex_api_gateway" "api_gw" {
     spec = <<-EOT
         openapi: "3.0.0"
         info:
-            version: 1.0.0
-            title: Face Recognizer API
+          version: 1.0.0
+          title: Face Recognizer API
         paths:
-            /:
-                get:
-                    summary: Get images with faces by key
-                    parameters:
-                        - name: face
-                            in: query
-                            description: Key of the object with face image 
-                            required: true
-                            schema:
-                                type: string
-                    x-yc-apigateway-integration:
-                        type: object_storage
-                        object: "{face}"
-                        bucket: ${yandex_storage_bucket.faces_bucket.bucket}
-                        service_account_id: ${yandex_iam_service_account.sa_face_recognizer.id}
+          /:
+            get:
+              summary: Get images with faces by key
+              parameters:
+                - name: face
+                  in: query
+                  description: Key of the object with face image 
+                  required: true
+                  schema:
+                    type: string
+              x-yc-apigateway-integration:
+                type: object_storage
+                object: "{face}"
+                bucket: ${yandex_storage_bucket.faces_bucket.bucket}
+                service_account_id: ${yandex_iam_service_account.sa_face_recognizer.id}
     EOT
 }
