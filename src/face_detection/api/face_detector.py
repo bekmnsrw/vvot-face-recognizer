2d4fea5356b2fdf9aca0f1c8f849fbebef36a53a
bekmnsrw <ilya.bekmansurov@gmail.com>
2025-02-09 23:26:04 +0300

    Another minor changes

--- src/face_detection/api/face_detector.py
+++ src/face_detection/api/face_detector.py
@@ -2,31 +2,32 @@ import cv2
 
 def get_faces_coordinates(image_path):
     image = cv2.imread(image_path)
-    max_width, max_height = 1920, 1080
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt2.xml")
+    max_allowed_width, max_allowed_height = 1920, 1080
 
-    original_height, original_width = image.shape[:2]
+    original_image_height, original_image_width = image.shape[:2]
 
-    if original_width > max_width or original_height > max_height:
-        scaling_factor = min(max_width / original_width, max_height / original_height)
+    if original_image_width > max_allowed_width or original_image_height > max_allowed_height:
+        scaling_factor = min(max_allowed_width / original_image_width, max_allowed_height / original_image_height)
         image = cv2.resize(image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
         additional_scaling_factor = 0.90
-        new_width = int(image.shape[1] * additional_scaling_factor)
-        new_height = int(image.shape[0] * additional_scaling_factor)
-        image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+        scaled_width, scaled_height = int(image.shape[1] * additional_scaling_factor), int(image.shape[0] * additional_scaling_factor)
+        image = cv2.resize(image, (scaled_width, scaled_height), interpolation=cv2.INTER_AREA)
 
-    resized_height, resized_width = image.shape[:2]
+    resized_image_height, resized_image_width = image.shape[:2]
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    original_faces = []
-    
-    for (x, y, w, h) in faces:
-        x_original = int(x * (original_width / resized_width))
-        y_original = int(y * (original_height / resized_height))
-        w_original = int(w * (original_width / resized_width))
-        h_original = int(h * (original_height / resized_height))
-        original_faces.append([x_original, y_original, w_original, h_original])
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt2.xml")
 
-    return original_faces
+    detected_faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+    original_scale_faces = []
+
+    for (x, y, width, height) in detected_faces:
+        x_original = int(x * (original_image_width / resized_image_width))
+        y_original = int(y * (original_image_height / resized_image_height))
+        width_original = int(width * (original_image_width / resized_image_width))
+        height_original = int(height * (original_image_height / resized_image_height))
+        original_scale_faces.append([x_original, y_original, width_original, height_original])
+
+    return original_scale_faces
